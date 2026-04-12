97500aa0d8cad93533ffe7608502cd9d93461bac
Gracesvg <134380789+Gracesvg@users.noreply.github.com>
2026-04-12 17:22:53 +0300

    Complete the search component using useState and useEffect.

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,19 @@ import TemperatureToggle from './components/TemperatureToggle.jsx';
 import ErrorMessage from './components/ErrorMessage.jsx';
 import WeatherCard from './components/WeatherCard.jsx';
 import Forecast from './components/Forecast.jsx';
+import { getCurrentWeather } from '../services/WeatherAPI.js';
+import { useWeather } from './hooks/useWeather.js';
 
 function App() {
+ const {currentWeather, 
+        forecast, 
+        loading, 
+        error,
+       unit, 
+       fetchWeatherByCity,
+       fetchWeatherByLocation,
+       toggleUnit
+      } = useWeather();
   return (
     <div className='min-h-screen relative overflow-hidden'>
       <div className='absolute inset-0 bg-cover bg-center bg-no-repeat' 
@@ -21,36 +32,43 @@ function App() {
           <div className='text-center mb-12'>
             <div className='mb-8'>
               <h1 className='text-5xl md:text-7xl font-bold text-white mb-4 drop-shadow-2xl tracking-tight '>
-                Weather{" "}
+                Weather
                 <span className='bg-gradient-to-r from-blue-400 to-purple-400 bg-clip-text text-transparent'>Pro</span>
               </h1>
               <p className="text-white/80 text-lg md:text-xl mb-8 max-w-2xl mx-auto leading-relaxed">Experience weather like never before with the best quality insights and predictions.</p>
             </div>
 
             <div className='flex flex-col lg:flex-row items-center justify-center space-y-6 lg:space-x-6 mb-12'>
-                <SearchBar />
+                <SearchBar onSearch = {fetchWeatherByCity} onLocationSearch = {fetchWeatherByLocation} location = {loading}/>
                <TemperatureToggle />
             </div>
           </div>
           <div className='space-y-8'>
-            <div className='flex justify-center'>
-              <div className='bg-white/10 backdrop-blur-xl rounded-3xl p-8 border border-white/20'>
-                <LoadingSpinner/>
-                <p className='text-white/80 text-center mt-4 font-medium '>Fetching latest weather data.....</p>
+            {loading && (
+              <div className='flex, justify-center'>
+                <div className='bg-white/10 backdrop-blur-xl rounded-3xl p-8 border border-white/20'>
+                  <LoadingSpinner/> 
+                  <p className='text-white/80 text-center mt-4 font-medium'>Fetch latest weather data</p>
+                </div>
               </div>
-            </div>
-            <div className='max-w-2xl mx-auto'>
+            )}
+            
+            {error && !loading && (
+              <div className='max-w-2xl mx-auto'>
                 <ErrorMessage/>
             </div>
+            )}
 
-            <div className='grid grid-cols-1 xl:grid-cols-3 gap-8'>
+            {currentWeather && !loading && (
+              <div className='grid grid-cols-1 xl:grid-cols-3 gap-8'>
               <div className='xl:col-span-2 '>
                 <WeatherCard/>
               </div>
               <div className='xl:col-span-1'>
-                <Forecast/>
+                  {forecast && <WeatherForecast/>}
               </div>
             </div>
+            )}
           </div>
 
         </div>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,73 @@
 import { MapPin, X } from 'lucide-react'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { SearchCities } from '../../services/WeatherAPI';
 
-function SearchBar() {
+function SearchBar(onSearch, onLocationSearch, loading) {
+    const [query, setQuery] = useState('');
+    const [suggestion, setSuggestion] = useState([]);
+    const [showSuggestion, setShowSuggestion] = useState(false);
+    const [searchLoading, setSearchLoading] = useState(false);
+
+    const searchRef = useRef();
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if(searchRef.current && !searchRef.current.contain(event.target)) {
+                setShowSuggestion(false);
+            }
+        }
+        document.addEventListener('mousedown', handleClickOutside)
+        return() => document.removeEventListener('mousedown', handleClickOutside)
+
+    }, [])
+
+    useEffect(() => {
+        const searchTimeOut = setTimeout(() => {
+            if (query.length > 2) {
+                setSearchLoading(true);
+                try {
+                    const result = await SearchCities(query);
+                    setSuggestion(result)
+                    setShowSuggestion(true)
+                } catch(err) {
+                    console.error('Search failed;', err) 
+                } finally {
+                    setSearchLoading(false);
+                }
+            } else {
+                setSuggestion([]);
+                setShowSuggestion(false);
+            }
+        }, 300)
+
+        return() => clearTimeout(searchTimeOut)
+    }, [query])
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (query.trim()) {
+            onSearch(query.trim())
+            setQuery("")
+            setShowSuggestion(false)
+        }
+    }
+    const clearSearch = (e) => {
+        e.preventDefault();
+        if (query.trim()) {
+            setQuery("")
+            setSuggestion([])
+            setShowSuggestion(false)
+        }
+    }
+    const handleSuggestionsClick = (city) => {
+        const cityName = city.name ? `${city.name}, ${city.state}`: city.name;
+            onSearch(cityName);
+            setQuery("");
+            setShowSuggestion(false);
+        }
+    }
+
   return 
-    <div className='relative w-full max-w-2xl'>
+    <div className='relative w-full max-w-2xl' ref={searchRef}>
         <form className="relative">
             <div className='relative group'>
                 <search className='absolute left-4 top-1/2 transform -translate-y-1/2 
@@ -12,35 +76,45 @@ function SearchBar() {
                 <input type="text" 
                 className="w-full pl-12 pr-24 py-4 bg-white/10
                 backdrop-blur-xl border border-white/20 rounded-2xl text-white placeholder-white/50 focus:outline-none focus:ring-2 
-                focus:ring-white/30 focus:border-white/40 transition-all duratino-300 hover:bg-white/15" 
-                placeholder="search for any city worldwide...."/>
+                focus:ring-white/30 focus:border-white/40 transition-all duratino-300 hover:bg-white/15 z-10" 
+                disabled = {loading}
+                placeholder="search for any city worldwide...."
+                onChange={(e) => setQuery(e.target.value)}
+                value={query}/>
                 
-                <button className='absolute right-14 top-1/2 transform -translate-y-1/2 text-white/50 hover:text-white transition-all p-1
-                rounded-full hover:bg-white/10 '>
+                {query &&(<button className='absolute right-14 top-1/2 transform -translate-y-1/2 text-white/50 hover:text-white transition-all p-1
+                rounded-full hover:bg-white/10 ' onClick={clearSearch}>
                     <X className='w-4 h-4'/>
-                </button>
+                </button>)}
+
                  <button className='absolute right-4 top-1/2 transform -translate-y-1/2 text-white/50 hover:text-white transition-all p-1
-                rounded-full hover:bg-white/10 '>
+                rounded-full hover:bg-white/10 ' onClick={onLocationSearch} disabled = {loading}>
                     <MapPin className='w-5 h-5'/>
                 </button>
             </div>
         </form>
 
-        <div className='absolute top-full left-0 right-0 mt-3 bg-white/10 backdrop-blur-xl border border-white/20 rounded-2xl shadow-2xl 
+        { showSuggestion && (suggestion.length > 0 || searchLoading)(<div className='absolute top-full left-0 right-0 mt-3 bg-white/10 backdrop-blur-xl border border-white/20 rounded-2xl shadow-2xl 
         overflow-hidden z-50 '>
-            <div className='p-6 text-center text-white/70'>
+            { searchLoading? (<div className='p-6 text-center text-white/70'>
                 <div className='animate-spin rounded-full h-6 w-6 border-2 border-white/30 border-t-white mx-auto '></div>
                     <p>Search cities...</p>
             </div>
-            <button className='w-full px-6 py-4 text-left hover:bg-white/10 transition-all duration-200 flex items-center justify-between group border-b
-            border-white/10 last:border-b-0 '>
+            ) :  (
+                suggestion.map((city, index) => {
+                    <button className='w-full px-6 py-4 text-left hover:bg-white/10 transition-all duration-200 flex items-center justify-between group border-b
+            border-white/10 last:border-b-0 ' key = {`${city.name}- ${city.country}- ${index}`} onClick={() => handleSuggestionsClick(city)}>
                 <div className='font-medium text-white group-hover:text-white/90 '>City name 
-                    <span>,city state</span>
+                    <span>,{city.state}</span>
                 </div>
-                <div className='text-sm text-white/60'>County</div>
+                <div className='text-sm text-white/60'>,{city.Country}</div>
                 <search className='w-4 h-4 text-white/40 group-hover:text-white/60 transition-all'/>
             </button>
-        </div>
+                })
+            )}
+
+            
+        </div>)}
     </div>
   
 }
